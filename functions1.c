4f1bca2e4397fae957f653ee1b7ac23e38b433b5
Dan Hug <dhug@albany.edu>
2017-10-20 17:57:41 -0400

    Add files via upload
    10/20/17 final product of alumni work session

--- functions1.c
+++ functions1.c
@@ -3,9 +3,22 @@ Daniel Hug dhug@albany.edu: Recoder
 Alana Ruth Aruth@albany.edu : Monitor
 Jessica Kanczura jKanczura@albany.edu : Leader
 */
+#include "constants.h"
+#include <stdio.h>
+//#define BYTE_SIZE 4
+//Creates archive file from a set of individual files.
+ void archive(char** fileNames, int numFiles, char* archiveName){
+ 	int i;
+ 	FILE* fp = fopen(archiveName, "w+");
+ 	//try to open(create) archive file to w+
+ 	
+ 	fwrite(&numFiles, BYTE_SIZE, 1, fp);
+ 	//fwrite to binary file the N(numfiles 4 Bytes)
+ 	for(i=0; i<numFiles; i++){
+ 		int length = strlen(*fileNames[i]);
+ 		fwrite(&length, BYTE_SIZE, 1, fp);
 
- void archive(char** filenames, int numFiles, char* archivename){
-
+ 	}
 
  }
 
--- functions2.c
+++ functions2.c
@@ -3,10 +3,36 @@ Daniel Hug dhug@albany.edu: Recoder
 Alana Ruth Aruth@albany.edu : Monitor
 Jessica Kanczura jKanczura@albany.edu : Leader
 */
-void printFileInfo(char* archivefile){
+#include "constants.h"
 
+//use stats 
+void printFileInfo(char* archiveFile){
+
+FILE *fp = fopen(archiveFile,"rb");
+long siz;
+char *result;
+if (fp == NULL)
+return NULL;
+
+fseek(f,0,SEEK_END);
+sz = ftell(fp);
+fseek(f,0,SEEK_SET);
+result = calloc(1,siz+1);
+if (result) {
+fread(result,1,siz,f);
+}
+fclose(f);
+return result;
 }
 
-void isDamaged(char **fileNames,int numFiles , char* archivefile){
-	
+
+
+
+void isDamaged(char **fileNames, int numFiles , char* archiveFile){
+
 }
+/*
+int tryOpen(FILE* fp){
+
+	if(fopen(fp, "r") )
+}*/
