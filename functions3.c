4eb19d148b9adbbe6feb8304096d86514d0e83b8
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-10-20 22:31:42 -0400

    Update functions3.c

--- functions3.c
+++ functions3.c
@@ -3,3 +3,18 @@ Daniel Hug dhug@albany.edu: Recoder
 Alana Ruth Aruth@albany.edu : Monitor
 Jessica Kanczura jKanczura@albany.edu : Leader
 */
+
+#include <stdio.h>
+
+int getFileSize(FILE *fp){
+  if (fp != NULL){
+    if (fseek(fp, 0L, SEEK_END)){
+      return -1;
+    }
+
+    rewind(fp);
+    return ftell(fp);
+  }
+
+  return -1;
+}
