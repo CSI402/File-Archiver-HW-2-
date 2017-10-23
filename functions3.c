f88aa9564be53ba0711c41ac4908cbd9545cc70d
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-10-23 11:22:22 -0400

    Update functions3.c

--- functions3.c
+++ functions3.c
@@ -7,13 +7,16 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 #include <stdio.h>
 
 int getFileSize(FILE *fp){
+  int len = -2;
+
   if (fp != NULL){
-    if (fseek(fp, 0L, SEEK_END)){
+    if (fseek(fp, 0L, SEEK_END) == -1L){
       return -1;
     }
 
+    len = ftell(fp);
     rewind(fp);
-    return ftell(fp);
+    return len;
   }
 
   return -1;
