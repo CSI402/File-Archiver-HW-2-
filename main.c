26dd818a1b7079b7083723b65c87dda1077b9ef2
alanaruth007 <32655088+alanaruth007@users.noreply.github.com>
2017-10-29 23:26:09 -0400

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,9 @@ Jessica Kanczura jKanczura@albany.edu : Leader
 #include "prototypes2.h"
 #include "constants.h"
 #include <string.h>
+#include <ctype.h>
 
+int isNumber(char number[]);
 
 int main(int argc, char *argv[]){
 	//If there are no arguments present besides the executable version name, print error and stop
@@ -18,6 +20,9 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
+	//Create a double pointer representing the set of individual files
+	char **files
+	
 	//If the flag (argv[1]) is "-a"
 	if (strcmp(argv[1],"-a") == 0){
 		//Must be at least 4 arguments with "-a" flag 
@@ -25,10 +30,16 @@ int main(int argc, char *argv[]){
 			fprintf(stderr, "Error: Inavlid number of arguments with \"-a\" flag\n");
 			return 0;
 		}
-		//Create a double pointer representing the set of individual files
-		char **files = argv + 3;
-		//Otherwise, create an archive file from the set of individual files
-		archive(files, (argc-3), argv[2]);
+		if (isNumber(argv[3])){
+   		   files = argv + 4;
+   		   archiveSpecial(files, (argc-4), argv[2], (int)argv[3]);
+ 		   }
+  		  //Otherwise, create an archive file from the set of individual files
+ 	        else{
+  	          //Create a double pointer representing the set of individual files
+	          files = argv + 3;
+  		  archive(files, (argc-3), argv[2]);
+  		}
 	}
 
 	//If the flag (argv[1]) is "-u"
@@ -71,4 +82,16 @@ int main(int argc, char *argv[]){
 	}
 	
 	return 0;
+}
+
+int isNumber(char number[]){
+  int i = 0;
+
+  if (number[0] == '-')
+    i = 1;
+  for (; number[i] != 0; i++){
+    if (!isdigit(number[i]))
+      return FALSE;
+  }
+  return TRUE;
 }
